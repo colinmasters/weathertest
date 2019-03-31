90fa1ffe9c6113695a76b1bd61006b000f61deeb
Colin Masters <colin.p.masters@gmail.com>
2019-03-31 11:03:02 -0700

    Initial Commit

---
+++ WeatherSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherSearch
+//
+//  Created by Colin Masters on 31/03/2019.
+//  Copyright © 2019 Colin Masters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
